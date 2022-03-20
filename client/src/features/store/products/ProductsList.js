7826eaf511e0fdb1a113ebe4b592f82204ab272a
alexeim <alexeim.dev@gmail.com>
2022-03-20 13:47:49 +0200

    Added filtering products

--- client/src/features/store/products/ProductsList.js
+++ client/src/features/store/products/ProductsList.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { editProduct, deleteProduct } from './productsSlice';
 import ProductItem from './ProductItem';
@@ -7,7 +7,12 @@ import styles from './ProductsList.module.scss';
 export default function ProductsList(props) {
 
     const dispatch = useDispatch();
-    const products = useSelector(state => state.products.products);
+
+    const [filter, setFilter] = useState();
+
+    const products = useSelector(state => filter 
+        ? state.products.products.filter(p => p.name.includes(filter) || p.description.includes(filter))
+        : state.products.products);
     
     function handleDeleteProduct(productId) {
         dispatch(deleteProduct(productId));
@@ -17,8 +22,15 @@ export default function ProductsList(props) {
         dispatch(editProduct(productId));
     }
 
+    function handleFilterChange(e) {
+        setFilter(e.target.value);
+    }
+
     return (
         <div className={styles.container}>
+            <div>
+                <input type="search" value={filter} onChange={handleFilterChange} />
+            </div>
             <ul>
                 {products?.map(product => 
                     <ProductItem key={product.id}
